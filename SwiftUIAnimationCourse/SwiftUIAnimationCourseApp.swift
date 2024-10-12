21f52faa40a31d5a16d4a902baa8c6ea54f33c6e
Govind Mulgir <govind.mulgir.17@gmail.com>
2024-10-12 22:33:55 +0530

    Initial Commit

---
+++ SwiftUIAnimationCourse/SwiftUIAnimationCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationCourseApp.swift
+//  SwiftUIAnimationCourse
+//
+//  Created by Govind on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
